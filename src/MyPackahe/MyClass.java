de9db03915b8c673fbb8085274e3045242e51d7b
pridestrike <ernestea@gmail.com>
2017-10-11 02:31:43 -0500

    Just commit

--- src/MyPackahe/MyClass.java
+++ src/MyPackahe/MyClass.java
@@ -1,4 +1,21 @@
 package MyPackahe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MyClass {
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        Collections.addAll(list1, 1, 5, 6, 11, 3, 15, 7, 8);
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        Collections.addAll(list2, 1, 8, 6, 21, 53, 5, 67, 18);
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.addAll(list1);
+        result.addAll(list2);
+        for (Integer x : result) {
+            System.out.println(x);
+        }
+    }
 }
