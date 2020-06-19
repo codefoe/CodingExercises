c024bd84363c303940d9971c3fe8e03a88f83ce6
codefoe <32694961+codefoe@users.noreply.github.com>
2020-06-19 12:38:16 -0500

    Update Blank.java

--- src/MyPackahe/Blank.java
+++ src/MyPackahe/Blank.java
@@ -1,14 +1,11 @@
 package MyPackahe;
 
-//import com.sun.tools.javac.jvm.StringConcat;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Blank {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        //int i=input.nextInt();
         String l="hello world";//first index inclusive, second index is exclusive
         String h=l.substring(0,1).toUpperCase()+l.substring(1,6)+" " +l.substring(6,7).toUpperCase()+l.substring(7);
         String pass="  hide  ";
@@ -22,13 +19,5 @@ public class Blank {
         System.out.println(" hi\t whats up");//tab insert
         System.out.println(" Hi\n whats up");//new line
         System.out.println(":" + passTrimmed);
-
-
-        //String s=input.nextLine();
-        //System.out.println((":"+i));
-        //System.out.println(":" +s);
     }
-   /* public static String fullName(String l,String m,String n){
-        return l.concat(m.concat(n));
-    }*/
 }
